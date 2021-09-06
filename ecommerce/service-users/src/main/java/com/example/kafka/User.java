da0e3f4b29834030500c6a718692b5dc27a05847
Lucas Mauro <lucas_de_mauro@hotmail.com>
2021-09-06 00:30:00 -0300

    Generalização de processo de batch assincrono e http fast delegate

---
+++ ecommerce/service-users/src/main/java/com/example/kafka/User.java
@@ -0,0 +1,14 @@
+package com.example.kafka;
+
+public class User {
+
+    private final String uuid;
+
+    public User(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+}
